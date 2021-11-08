8b81841a3094b54c64fb6b64b7bdfb509f5d1824
Khadija salak <66210469+khadijaSalak@users.noreply.github.com>
2021-11-09 00:13:41 +0100

    Update huffman.java

--- huffman.java
+++ huffman.java
@@ -5,6 +5,16 @@ public class Huffman {
     }
 public static void main(String[] args)
     {
-  
+      Scanner s = new Scanner(System.in);
+
+		// number of characters.
+		int n = 6;
+		char[] charArray = { 'a', 'b', 'c', 'd', 'e', 'f' };
+		int[] charfreq = { 5, 9, 12, 13, 16, 45 };
+
+		// creating a priority queue q.
+		// makes a min-priority queue(min-heap).
+		PriorityQueue<HuffmanNode> q
+			= new PriorityQueue<HuffmanNode>(n, new MyComparator());
     }
 }
